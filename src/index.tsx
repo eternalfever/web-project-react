8cc826933c2aa9ac47b898f1ec96c7d5db50a00b
eternalfever <anastasia.kuzminaaa@gmail.com>
2022-06-08 10:53:37 +0300

    Проблема

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
+import "../src/styles/style.css";
+import App from './App';
+import { MoralisProvider } from "react-moralis";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+
+ReactDOM.render(
+    <React.StrictMode>
+        <MoralisProvider serverUrl="https://jrffsta5kgpz.usemoralis.com:2053/server" appId="RHdGOpDM94gMymPyI6IRXBRGp5mybwmQibCkdn5m">
+            <App />
+        </MoralisProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+ );
